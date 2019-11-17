385a812e67e3d23cf1d3aa43808c7f9efb01906e
Ubuntu <ubuntu@ip-172-31-27-175.us-east-2.compute.internal>
2019-11-17 08:31:04 +0000

    commit

--- main/app.js
+++ main/app.js
@@ -1,4 +1,45 @@
+// Create an object type UserException
+function Error(message) {
+  this.message = message;
+  this.name = 'UserException';
+}
+
+
 function addTokens(input, tokens){
+    
+    if (typeof input == 'string' || input instanceof String) {
+        
+        if(input.length<6){
+            
+            throw new Error('Input should have at least 6 characters')
+        }else
+        {
+            
+            if (tokens instanceof Array && typeof tokens.isArray === 'string') {
+                if(!(input.includes("...")))
+                {
+                    
+                    return tokens;
+                }
+                
+            }else
+            {
+                throw new Error('Invalid array format')
+            }
+            
+            
+        }
+   
+} 
+else {
+   
+ throw new Error('Invalid input');
+   
+   
+}
+    
+    
+    
     
 }
 
